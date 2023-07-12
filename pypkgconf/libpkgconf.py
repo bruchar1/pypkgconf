3e4656020ae865fb3514d22f49fb0a00285459cd
Charles Brunet <charles.brunet@optelgroup.com>
2023-07-12 10:46:48 -0400

    fix many bugs to make it work with meson

--- pypkgconf/libpkgconf.py
+++ pypkgconf/libpkgconf.py
@@ -7,13 +7,18 @@ from dataclasses import dataclass, field, fields
 from contextlib import contextmanager
 import logging
 import os
+import sys
 import typing as T
 
+
+logger = logging.getLogger(__name__)
 
 @ffi.def_extern()
 def error_handler(msg: str, client, data) -> bool:
     """ This is a Python callback to handle errors """
-    logging.error(msg.rstrip())
+    log_level = ffi.from_handle(data)
+
+    logger.log(log_level, ffi.string(msg).decode(errors='replace').rstrip())
     return True
 
 
@@ -94,12 +99,32 @@ class NodeIter:
 @dataclass(kw_only=True)
 class PkgconfFlags:
 
+    define_prefix: bool = True if sys.platform == 'win32' else False
+    skip_root_virtual: bool = False
     static: bool = False
+
+    debug: bool = False
+    maximum_traverse_depth = 2000
+
+    def __post_init__(self):
+        if os.environ.get('PKG_CONFIG_DONT_DEFINE_PREFIX'):
+            self.define_prefix = False
+
+        if os.environ.get('PKG_CONFIG_EARLY_TRACE'):
+            self.debug = True
+        
+        mtd = os.environ.get("PKG_CONFIG_MAXIMUM_TRAVERSE_DEPTH")
+        if mtd is not None:
+            self.maximum_traverse_depth = int(mtd)
 
     @property
     def flags(self) -> int:
         f = flags.PKGF_NONE
 
+        if self.define_prefix:
+            f |= flags.PKGF_REDEFINE_PREFIX
+        if self.skip_root_virtual:
+            f |= flags.PKGF_SKIP_ROOT_VIRTUAL
         if self.static:
             f |= flags.PKGF_SEARCH_PRIVATE | flags.PKGF_MERGE_PRIVATE_FRAGMENT
         
@@ -116,27 +141,82 @@ class PkgconfFlags:
 
 class PkgconfClient:
 
-    def __init__(self, define_variables: T.Optional[T.Dict[str, str]] = None, **kwargs):
+    def __init__(self, define_variables: T.Optional[T.Dict[str, str]] = None,
+                 with_paths: T.Optional[T.List[str]] = None, **kwargs):
         self._personality = lib.pkgconf_cross_personality_default()
-        self._client = lib.pkgconf_client_new(lib.error_handler, ffi.NULL, self._personality)
+        if with_paths:
+            self.__add_paths(with_paths)
 
-        self._packages = {}
+        self._options = PkgconfFlags(**kwargs)
+        self.__init_client(self._options.debug)
+
         self._variables = {}
         if define_variables:
             self.define_variables(**define_variables)
 
-        self._options = PkgconfFlags(**kwargs)
         lib.pkgconf_client_set_flags(self._client, self._options.flags)
 
         # at this point, want_client_flags should be set, so build the dir list
         lib.pkgconf_client_dir_list_build(self._client, self._personality)
+
+    def __init_client(self, debug: bool):
+        self.__debug_level = ffi.new_handle(logging.DEBUG)
+        self.__error_level = ffi.new_handle(logging.ERROR)
+
+        self._client = lib.pkgconf_client_new(lib.error_handler, self.__error_level, self._personality)
+        if debug:
+            lib.pkgconf_client_set_trace_handler(self._client, lib.error_handler, self.__debug_level)
+
+    def __add_paths(self, paths: T.List[str]):
+        dir_list = ffi.new('pkgconf_list_t *')
+        for p in paths:
+            lib.pkgconf_path_add(p.encode(), dir_list, True)
+        lib.pkgconf_path_copy_list(ffi.addressof(self._personality.dir_list), dir_list)
+        lib.pkgconf_path_free(dir_list)
+
+    def __getstate__(self) -> object:
+        d = self.__dict__.copy()
+
+        d['_paths'] = []
+        for p in NodeIter(self._personality.dir_list, 'pkgconf_path_t *'):
+            d['_paths'].append(ffi.string(p.path).decode())
+
+        sysroot = lib.pkgconf_client_get_sysroot_dir(self._client)
+        if sysroot != ffi.NULL:
+            d['_sysroot'] = ffi.string()
+        del d['_client']
+        del d['_personality']
+        del d['_PkgconfClient__error_level']
+        del d['_PkgconfClient__debug_level']
+        return d
+
+    def __setstate__(self, d: T.Dict) -> None:
+        self._personality = lib.pkgconf_cross_personality_default()
+        self.__add_paths(d['_paths'])
+        del d['_paths']
+
+        sysroot = d.get('_sysroot')
+        if sysroot:
+            del d['_sysroot']
+
+        self.__dict__.update(d)
+        self.__init_client(self._options.debug)
+
+        if sysroot:
+            self.set_sysroot(sysroot)
+
+        if self._variables:
+            self.define_variables(**self._variables)
+
+        lib.pkgconf_client_set_flags(self._client, self._options.flags)
+        lib.pkgconf_client_dir_list_build(self._client, self._personality)
     
     def __del__(self):
-        for pkg in self._packages.values():
-            lib.pkgconf_pkg_unref(self._client, pkg)
-
-        lib.pkgconf_cross_personality_deinit(self._personality)
-        lib.pkgconf_client_free(self._client)
+        if hasattr(self, '_personality'):
+            lib.pkgconf_cross_personality_deinit(self._personality)
+        
+        if hasattr(self, '_client'):
+            lib.pkgconf_client_free(self._client)
 
     def version(self) -> T.Optional[str]:
         return self.modversion('pkgconf')
@@ -152,6 +232,9 @@ class PkgconfClient:
             lib.pkgconf_cache_free(self._client)
             return True
         return False
+    
+    def set_sysroot(self, sysroot: str) -> None:
+        lib.pkgconf_client_set_sysroot_dir(sysroot.encode())
         
     @contextmanager
     def variables_ctx(self, **kwargs):
@@ -174,92 +257,114 @@ class PkgconfClient:
         if modified:
             lib.pkgconf_client_set_flags(self._client, current_flags)
             lib.pkgconf_cache_free(self._client)
+    
+    @contextmanager
+    def _solve(self, packages: T.List[str], maximum_traverse_depth=None):
+        lib.pkgconf_cache_free(self._client)
 
-    def _hash(self) -> int:
-        """Return a hash for the current state"""
-        return hash(tuple(sorted(self._variables.items())))
+        world = ffi.new('pkgconf_pkg_t *')
+        id = ffi.new('char[]', 'virtual:world'.encode())
+        world.id = id
+        realname = ffi.new('char[]', 'virtual world package'.encode())
+        world.realname = realname
+        world.flags = flags.PROPF_STATIC | flags.PROPF_VIRTUAL
+    
+        pkgq = ffi.new("pkgconf_list_t *")
+        for p in packages:
+            if not p:
+                continue
+            lib.pkgconf_queue_push(pkgq, p.encode())
 
-    def _get(self, package: str):
-        key = (package, self._hash())
-        if key not in self._packages:
-            pkg = lib.pkgconf_pkg_find(self._client, package.encode())
-            if pkg is None:
-                lib.pkgconf_error(self._client, f"Package '{package}' was not found\n")
-            self._packages[key] = pkg
+        if maximum_traverse_depth is None:
+            maximum_traverse_depth = self._options.maximum_traverse_depth
+        r = lib.pkgconf_queue_solve(self._client, pkgq, world, maximum_traverse_depth)
+        
+        yield world if r else None
 
-        return self._packages[key]
+        lib.pkgconf_solution_free(self._client, world)
+        lib.pkgconf_queue_free(pkgq)
+
+    def _iter_world(self, packages: T.List[str], maximum_traverse_depth=None):
+        with self._solve(packages, maximum_traverse_depth) as world:
+            if world:
+                for pkg_dep in NodeIter(world.required, 'pkgconf_dependency_t *'):
+                    yield pkg_dep.match
 
     def modversion(self, package: str) -> T.Optional[str]:
-        pkg = self._get(package)
-        if pkg is None:
-            return None
-        return ffi.string(pkg.version).decode()
+        version = []
+        
+        for pkg in self._iter_world([package], maximum_traverse_depth=1):
+            if pkg.version != ffi.NULL:
+                version.append(ffi.string(pkg.version).decode())
+        
+        return "\n".join(version) if version else None
 
-    def cflags(self, package: str, **kwargs) -> T.Optional[str]:
-        pkg = self._get(package)
-        if pkg is None:
-            return None
-
-        unfiltered_list = ffi.new('pkgconf_list_t *')
-        filtered_list = ffi.new('pkgconf_list_t *')
-
-        try:
-            eflag = lib.pkgconf_pkg_cflags(self._client, pkg, unfiltered_list, 2)
-            if eflag != flags.ERRF_OK:
+    def cflags(self, packages: T.Union[str, T.List[str]], **kwargs) -> T.Optional[str]:
+        if isinstance(packages, str):
+            packages = [packages]
+        with self._solve(packages) as world:
+            if not world:
                 return None
             
-            kwargs.setdefault('keep_system', os.environ.get('PKG_CONFIG_ALLOW_SYSTEM_CFLAGS', False))
-            data = ffi.new_handle(CflagFilterData(**kwargs))
-            lib.pkgconf_fragment_filter(self._client, filtered_list, unfiltered_list, lib.filter_cflags, data)
+            unfiltered_list = ffi.new('pkgconf_list_t *')
+            filtered_list = ffi.new('pkgconf_list_t *')
 
-            result_str = lib.pkgconf_fragment_render(filtered_list, False, ffi.NULL)
-            return ffi.string(result_str).decode()
-        
-        finally:
-            lib.pkgconf_fragment_free(filtered_list)
-            lib.pkgconf_fragment_free(unfiltered_list)
+            try:
+                eflag = lib.pkgconf_pkg_cflags(self._client, world, unfiltered_list, 2)
+                if eflag != flags.ERRF_OK:
+                    return None
+                
+                kwargs.setdefault('keep_system', os.environ.get('PKG_CONFIG_ALLOW_SYSTEM_CFLAGS', False))
+                data = ffi.new_handle(CflagFilterData(**kwargs))
+                lib.pkgconf_fragment_filter(self._client, filtered_list, unfiltered_list, lib.filter_cflags, data)
 
-    def libs(self, package: str, static: bool = False, **kwargs) -> T.Optional[str]:
-        pkg = self._get(package)
-        if pkg is None:
-            return None
-
-        unfiltered_list = ffi.new('pkgconf_list_t *')
-        filtered_list = ffi.new('pkgconf_list_t *')
-
-        try:
-            eflag = lib.pkgconf_pkg_libs(self._client, pkg, unfiltered_list, 2)
-            if eflag != flags.ERRF_OK:
-                return None
+                result_str = lib.pkgconf_fragment_render(filtered_list, False, ffi.NULL)
+                return ffi.string(result_str).decode()
             
-            kwargs.setdefault('keep_system', os.environ.get('PKG_CONFIG_ALLOW_SYSTEM_LIBS', False))
-            data = ffi.new_handle(LibsFilterData(**kwargs))
-            lib.pkgconf_fragment_filter(self._client, filtered_list, unfiltered_list, lib.filter_libs, data)
+            finally:
+                lib.pkgconf_fragment_free(filtered_list)
+                lib.pkgconf_fragment_free(unfiltered_list)
 
-            result_str = lib.pkgconf_fragment_render(filtered_list, False, ffi.NULL)
-            return ffi.string(result_str).decode()
-        
-        finally:
-            lib.pkgconf_fragment_free(filtered_list)
-            lib.pkgconf_fragment_free(unfiltered_list)
+    def libs(self, packages: T.Union[str, T.List[str]], **kwargs) -> T.Optional[str]:
+        if isinstance(packages, str):
+            packages = [packages]
+        with self._solve(packages) as world:
+            if not world:
+                return None
+
+            unfiltered_list = ffi.new('pkgconf_list_t *')
+            filtered_list = ffi.new('pkgconf_list_t *')
+
+            try:
+                eflag = lib.pkgconf_pkg_libs(self._client, world, unfiltered_list, 2)
+                if eflag != flags.ERRF_OK:
+                    return None
+                
+                kwargs.setdefault('keep_system', os.environ.get('PKG_CONFIG_ALLOW_SYSTEM_LIBS', False))
+                data = ffi.new_handle(LibsFilterData(**kwargs))
+                lib.pkgconf_fragment_filter(self._client, filtered_list, unfiltered_list, lib.filter_libs, data)
+
+                result_str = lib.pkgconf_fragment_render(filtered_list, True, ffi.NULL)
+                return ffi.string(result_str).decode()
+            
+            finally:
+                lib.pkgconf_fragment_free(filtered_list)
+                lib.pkgconf_fragment_free(unfiltered_list)
 
     def variable(self, package: str, variable_name: str) -> T.Optional[str]:
-        pkg = self._get(package)
-        if pkg is None:
-            return None
+        found_vars = []
+
+        with self.options_ctx(skip_root_virtual=True):
+            for pkg in self._iter_world([package], maximum_traverse_depth=1):
+                var = lib.pkgconf_tuple_find(self._client, ffi.addressof(pkg.vars), variable_name.encode())
+                if var != ffi.NULL:
+                    found_vars.append(ffi.string(var).decode())
         
-        var = lib.pkgconf_tuple_find(self._client, ffi.addressof(pkg.vars), variable_name.encode())
-        if var == ffi.NULL:
-            return None
-        
-        return ffi.string(var).decode()
+        return ' '.join(found_vars) if found_vars else None
 
     def list_variables(self, package: str) -> T.Optional[T.List[str]]:
-        pkg = self._get(package)
-        if pkg is None:
-            return None
-        
         variables = []
-        for variable in NodeIter(pkg.vars):
-            variables.append(ffi.string(variable.key).decode())
-        return variables
+        for pkg in self._iter_world([package], maximum_traverse_depth=1): 
+            for variable in NodeIter(pkg.vars):
+                variables.append(ffi.string(variable.key).decode())
+        return variables or None
--- tests/testpypkgconf.py
+++ tests/testpypkgconf.py
@@ -1,6 +1,7 @@
 from pypkgconf import PkgconfClient
 
 import os
+import pickle
 import unittest
 
 
@@ -58,6 +59,29 @@ class TestLibPkgConf(unittest.TestCase):
             'pcfiledir',
             'prefix',
         }, set(client.list_variables('simple')))
+    
+    def test_modversion_nonexistent(self):
+        client = PkgconfClient()
+        self.assertIsNone(client.modversion('nonexistent'))
+
+    def test_with_paths(self):
+        env = os.environ.copy()
+        del os.environ['PKG_CONFIG_PATH']
+        datapath = os.path.join(os.path.dirname(__file__), 'data')
+        client = PkgconfClient(with_paths=[datapath])
+        
+        self.assertEqual('1.0.0', client.modversion('simple'))
+        os.environ = env
+
+    def test_pickle(self):
+        client = PkgconfClient()
+        self.assertEqual('1.0.0', client.modversion('simple'))
+
+        d = pickle.dumps(client)
+
+        c2 = pickle.loads(d)
+
+        self.assertEqual('1.0.0', c2.modversion('simple'))
 
     def test_list_no_variables(self):
         client = PkgconfClient()
@@ -135,6 +159,43 @@ class TestLibPkgConf(unittest.TestCase):
         client = PkgconfClient()
 
         self.assertEqual('-L/usr/lib -lsystem', client.libs('system', keep_system=True))
+
+    def test_circular_requires(self):
+        client = PkgconfClient()
+
+        self.assertEqual('-lcirc3 -lcirc2 -lcirc1', client.libs('circular-1'))
+    
+    def test_dependencies_shared(self):
+        datapath = os.path.join(os.path.dirname(__file__), 'data', 'dependencies')
+        client = PkgconfClient(with_paths=[datapath])
+
+        self.assertEqual('-lb_dep_c -la_dep_c -lc_dep', client.libs(['a_dep_c', 'b_dep_c']))
+        self.assertEqual('-lb_dep_c -la_dep_c -lc_dep', client.libs(['c_dep', 'a_dep_c', 'b_dep_c']))
+        self.assertEqual('-lb_dep_c -la_dep_c -lc_dep', client.libs(['a_dep_c', 'c_dep', 'b_dep_c']))
+        self.assertEqual('-lb_dep_c -la_dep_c -lc_dep', client.libs(['a_dep_c', 'b_dep_c', 'c_dep']))
+
+    def test_dependencies_redundancy(self):
+        datapath = os.path.join(os.path.dirname(__file__), 'data', 'dependencies')
+        client = PkgconfClient(with_paths=[datapath])
+
+        self.assertEqual('-lb_dep_c -lc_dep -la_dep_c', client.libs(['a_dep_c', 'a_dep_c', 'b_dep_c']))
+        self.assertEqual('-la_dep_c -lb_dep_c -lc_dep', client.libs(['b_dep_c', 'a_dep_c', 'b_dep_c']))
+
+    def test_dependencies_diamond(self):
+        datapath = os.path.join(os.path.dirname(__file__), 'data', 'dependencies')
+        client = PkgconfClient(with_paths=[datapath])
+
+        self.assertEqual('-le_dep_g_f -ld_dep_e_f -lg_dep -lf_dep_g', client.libs('d_dep_e_f'))
+        self.assertEqual('-le_dep_g_f -ld_dep_f_e -lg_dep -lf_dep_g', client.libs('d_dep_f_e'))
+
+    def test_dependencies_nested(self):
+        datapath = os.path.join(os.path.dirname(__file__), 'data', 'dependencies')
+        client = PkgconfClient(with_paths=[datapath])
+
+        self.assertEqual('-li_dep_k_j -lh_dep_k_i_j -lj_dep_k -lk_dep', client.libs('h_dep_k_i_j'))
+        self.assertEqual('-lh_dep_k_i_j -lj_dep_k -li_dep_k_j -lk_dep', client.libs(['h_dep_k_i_j', 'i_dep_k_j']))
+        self.assertEqual('-lh_dep_k_i_j -li_dep_k_j -lj_dep_k -lk_dep', client.libs(['k_dep', 'j_dep_k', 'i_dep_k_j', 'h_dep_k_i_j']))
+
 
 if __name__ == '__main__':
     unittest.main()
